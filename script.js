fa30922698225998c5e2512d81a65ce9ae937c40
Sandi Theresia <theresiasandi@gmail.com>
2022-04-18 18:19:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ var thehours = new Date().getHours();
 	}
 
 	$('.greeting').append(themessage);
+
+
+// needs jquery.min.js
+//$(document).ready(function(){
+	//var d = new Date();
+	//var n = d.getHours();
+	//if (n > 19 || n < 6)
+ 		// If time is after 7PM or before 6AM, apply night theme to ‘body’
+ 		//document.body.className = "night";
+	//else if (n > 16 && n < 19)
+ 		// If time is between 4PM – 7PM sunset theme to ‘body’
+ 		//document.body.className = "sunset";
+	//else
+ 		// Else use ‘day’ theme
+ 		//document.body.className = "day";
+//});
+
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+    
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
+    
+    var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
